2d34b027a6b83809378951c12a73876d2a27c033
heeeyi <e0772644@u.nus.edu>
2023-09-08 15:16:44 +0800

    Basic functionality of adding new questions (simple question now, not rich text)

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,14 @@ import path from "path"; // for reading file
 const app = express();
 const port = 3001;
 
+interface Question {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  complexity: string;
+}
+
 // Enable CORS for all routes (Not recommended for production)
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -14,6 +22,8 @@ app.use((req, res, next) => {
 
 app.use('/images', express.static(path.join(__dirname, 'data/images')));
 
+app.use(express.json())
+
 app.get('/questions', (req, res) => {
   fs.readFile(path.join(__dirname, 'data/sampleQuestions.json'), 'utf8', (err, data) => {
     if (err) {
@@ -22,6 +32,23 @@ app.get('/questions', (req, res) => {
     return res.json(JSON.parse(data));
   });
 });
+
+// allow addition into question bank
+app.post('/addQuestion', (req, res) => {
+  const newQuestion: Partial<Question> = req.body;
+  const questions: Question[] = JSON.parse(fs.readFileSync('data/sampleQuestions.json', 'utf8'));
+
+  // Generate a new ID for the question
+  newQuestion.id = questions.length ? Math.max(...questions.map((q: Question) => q.id)) + 1 : 1;
+
+  // Add the new question
+  questions.push(newQuestion as Question);
+
+  // Save back to the JSON file
+  fs.writeFileSync('data/sampleQuestions.json', JSON.stringify(questions, null, 2));
+  res.status(201).send('New question added');
+});
+
 
 
 app.listen(port, () => {
--- src/components/QuestionBank.tsx
+++ src/components/QuestionBank.tsx
@@ -14,6 +14,13 @@ const QuestionBank: React.FC = () => {
   const [questions, setQuestions] = useState<Question[]>([]); 
   const [expandedQuestionId, setExpandedQuestionId] = useState<number | null>(null);
 
+  const [newQuestion, setNewQuestion] = useState({
+    title: '',
+    description: '',
+    category: '',
+    complexity: ''
+  });
+
   useEffect(() => {
     fetch('http://localhost:3001/questions')
       .then((res) => res.json())
@@ -22,6 +29,23 @@ const QuestionBank: React.FC = () => {
 
   const toggleQuestionDetails = (id: number) => {
     setExpandedQuestionId(expandedQuestionId === id ? null : id);
+  };
+
+  // newQuestion only have 4 fields, unlike Question have 5, need to use Partial
+  const addQuestion = (newQuestion: Partial<Question>) => {
+    fetch('http://localhost:3001/addQuestion', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newQuestion),
+    })
+    .then(() => {
+      // Re-fetch questions to update UI
+      return fetch('http://localhost:3001/questions');
+    })
+    .then((res) => res.json())
+    .then((data) => setQuestions(data));
   };
 
   return (
@@ -60,6 +84,32 @@ const QuestionBank: React.FC = () => {
           ))}
         </tbody>
       </table>
+
+      {/* Create new Questions */}
+      <h2>Add a New Question</h2>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        addQuestion(newQuestion);
+      }}>
+        <div>
+          <label>Title</label>
+          <input type="text" value={newQuestion.title} onChange={e => setNewQuestion({ ...newQuestion, title: e.target.value })} />
+        </div>
+        <div>
+          <label>Description</label>
+          <textarea value={newQuestion.description} onChange={e => setNewQuestion({ ...newQuestion, description: e.target.value })}></textarea>
+        </div>
+        <div>
+          <label>Category</label>
+          <input type="text" value={newQuestion.category} onChange={e => setNewQuestion({ ...newQuestion, category: e.target.value })} />
+        </div>
+        <div>
+          <label>Complexity</label>
+          <input type="text" value={newQuestion.complexity} onChange={e => setNewQuestion({ ...newQuestion, complexity: e.target.value })} />
+        </div>
+        <button type="submit">Add Question</button>
+      </form>
+
     </div>
   );
 };
