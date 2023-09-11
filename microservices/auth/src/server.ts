d47b4e5d7f202609eea50013685eb968d54f9459
rexcyrio <stefancyrio@gmail.com>
2023-09-11 18:52:17 +0800

    wip: use user service database

--- microservices/auth/src/server.ts
+++ microservices/auth/src/server.ts
@@ -4,10 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import session from "express-session";
-import mongodb, { MongoClient, ServerApiVersion } from "mongodb";
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-import { UserDocument } from "./types";
 
 dotenv.config();
 
@@ -20,76 +18,55 @@ if (
   throw new Error("dotenv is not configured!");
 }
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGODB_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-(async function init() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("main-db").command({ ping: 1 });
-    console.log("Successfully connected to MongoDB!");
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
-      const users = client.db("main-db").collection("users");
-      const userDocument = await users.findOne({ username });
+      const response = await fetch(`/api/users/${username}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-      // check if user exists
-      if (userDocument === null) {
-        return done(null, false);
-      }
+      const json = await response.json();
 
       // check for correct password
-      const isPasswordMatch = await bcrypt.compare(
-        password,
-        userDocument.password
-      );
+      const isPasswordMatch = await bcrypt.compare(password, json.password);
 
       if (isPasswordMatch) {
-        return done(null, userDocument);
+        return done(null, json);
       }
 
       // wrong password
       return done(null, false);
     } catch (error) {
+      console.error(error);
       return done(error, false);
     }
   })
 );
 
-// convert {expressUser / userDocument} to id
+// convert {expressUser / json} to {id / username}
 passport.serializeUser((expressUser, done) => {
-  const userDocument = expressUser as UserDocument;
-  return done(null, userDocument._id);
+  const json = expressUser as any;
+  return done(null, json.username);
 });
 
-// convert id to {expressUser / userDocument}
-passport.deserializeUser(async (_id: string, done) => {
+// convert {id / username} to {expressUser / json}
+passport.deserializeUser(async (username: string, done) => {
   try {
-    const users = client.db("main-db").collection("users");
-    const userDocument = await users.findOne({
-      _id: new mongodb.ObjectId(_id),
+    const response = await fetch(`/api/users/${username}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
-    if (userDocument === null) {
-      return done(`User with _id="${_id}" does not exist`, false);
-    }
+    const json = await response.json();
 
-    return done(null, userDocument);
+    return done(null, json);
   } catch (error) {
+    console.error(error);
     return done(error, false);
   }
 });
@@ -121,41 +98,31 @@ app.get("/", (req, res) => {
 
 app.post("/api/auth/log-in", passport.authenticate("local"), (req, res) => {
   const expressUser = req.user;
-  const userDocument = expressUser as UserDocument;
+  const json = expressUser as any;
 
-  return res.status(200).send({ username: userDocument.username });
+  return res.status(200).send({ username: json.username });
 });
 
 app.post("/api/auth/sign-up", async (req, res) => {
   const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    // check whether this username already exists
-    const users = client.db("main-db").collection("users");
-    const userDocument = await users.findOne({ username });
+    await fetch(`/api/users/${username}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: hashedPassword,
+      }),
+    });
 
-    if (userDocument) {
-      // this username already exists
-      res.status(409).send("Duplicated username");
-      return;
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUserDocument = {
-      username,
-      password: hashedPassword,
-    };
-
-    const result = await users.insertOne(newUserDocument);
-
-    if (result.acknowledged) {
-      res.status(200).send();
-    } else {
-      throw new Error("Database failed to acknowledge request");
-    }
+    res.status(200).send();
   } catch (error) {
     console.error(error);
-    res.status(503).send(error);
+    res.send(error);
   }
 });
 
@@ -177,37 +144,31 @@ app.get("/api/auth/secret", passport.authenticate("local"), (req, res) => {
   res.send("secret");
 });
 
-app.post(
-  "/api/auth/change-password",
-  passport.authenticate("local"),
-  async (req, res) => {
-    const expressUser = req.user;
-    const userDocument = expressUser as UserDocument;
+app.put("/api/users", passport.authenticate("local"), async (req, res) => {
+  const expressUser = req.user;
+  const json = expressUser as any;
 
-    const { newPassword } = req.body;
-    const newHashedPassword = await bcrypt.hash(newPassword, 10);
+  const { newPassword } = req.body;
+  const newHashedPassword = await bcrypt.hash(newPassword, 10);
 
-    try {
-      const users = client.db("main-db").collection("users");
+  try {
+    await fetch(`/api/users/${json.username}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: json.username,
+        password: newHashedPassword,
+      }),
+    });
 
-      const result = await users.updateOne(
-        { _id: new mongodb.ObjectId(userDocument._id) },
-        {
-          $set: { password: newHashedPassword },
-        }
-      );
-
-      if (result.acknowledged) {
-        res.status(200).send();
-      } else {
-        throw new Error("Database failed to acknowledge request");
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(503).send(error);
-    }
+    res.status(200).send();
+  } catch (error) {
+    console.error(error);
+    res.send(error);
   }
-);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
