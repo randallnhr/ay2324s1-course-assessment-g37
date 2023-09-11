9a64480a03000695ee8d879c55482c5db7233e7c
gohyongjing <gohyongjing@gmail.com>
2023-09-11 14:13:20 +0800

    Clean up ignored files

--- microservices/questions-repo/dist/utility/statusCodes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CODE_INTERNAL_SERVER_ERROR = exports.CODE_NOT_FOUND = exports.CODE_BAD_REQUEST = exports.CODE_OK = void 0;
-exports.CODE_OK = 200;
-exports.CODE_BAD_REQUEST = 400;
-exports.CODE_NOT_FOUND = 404;
-exports.CODE_INTERNAL_SERVER_ERROR = 500;
