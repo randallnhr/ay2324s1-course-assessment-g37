261097d6df741e8c39cad86649a4651047c6148c
wz2k <87819279+wz2k@users.noreply.github.com>
2023-09-17 15:43:25 +0800

    Change endpoint name

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -3,6 +3,6 @@ import userRoutes from "./routes/user";
 
 const app = express();
 app.use(express.json());
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;
