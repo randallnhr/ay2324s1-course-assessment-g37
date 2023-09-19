f90b67ba11f6c87d3361d582604acd7fbda883d5
wz2k <87819279+wz2k@users.noreply.github.com>
2023-09-19 21:17:15 +0800

    Change port number

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,7 +1,7 @@
 import app from "./index";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3219;
 
-app.listen(port, () => {
+app.listen(Number(port), "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${port}`);
 });
