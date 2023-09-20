a7c891f80b888aba4cda14d1ba4074751306fe0e
heeeyi <e0772644@u.nus.edu>
2023-09-20 20:37:05 +0800

    Attempt to produce persistent data storage using Cookies

--- microservices/front-end/src/UserContext.tsx
+++ microservices/front-end/src/UserContext.tsx
@@ -1,5 +1,5 @@
 // should be able to carry both attribute & function
-import React, {useState, useContext, ReactNode, useEffect} from 'react';
+import React, { useState, useContext, ReactNode, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import { User } from './components/types';
 
@@ -21,21 +21,27 @@ interface UserProviderProps {
 export function UserProvider({ children }: UserProviderProps) {
     const [currentUser, setCurrentUser] = useState<User | null>(null);
 
-    // read cookie and update context
-    useEffect(() => {
+    const updateCurrentUserFromCookie = () => {
         const cookieValue = Cookies.get('currentUser');
         if (cookieValue) {
             setCurrentUser(JSON.parse(cookieValue));
         }
+    };
+
+    useEffect(() => {
+        window.addEventListener('load', updateCurrentUserFromCookie);
+        return () => {
+            window.removeEventListener('load', updateCurrentUserFromCookie);
+        };
     }, []);
-    
+
     const value: UserContextType = {
         currentUser,
-        setCurrentUser
+        setCurrentUser,
     };
 
     return (
-        <UserContext.Provider value = {value}>
+        <UserContext.Provider value={value}>
             {children}
         </UserContext.Provider>
     );
