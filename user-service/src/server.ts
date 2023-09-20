198cd338ba0f1fa0dc21a57c43f3963058e9348b
wz2k <87819279+wz2k@users.noreply.github.com>
2023-09-20 17:28:58 +0800

    Add additional server listener

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -2,6 +2,8 @@ import app from "./index";
 
 const port = process.env.PORT || 3219;
 
+app.listen(port);
+
 app.listen(Number(port), "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${port}`);
 });
