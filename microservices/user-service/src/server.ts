54c7a0916fc9f0f05cd495a168943ccb81b28d83
wz2k <87819279+wz2k@users.noreply.github.com>
2023-09-25 20:29:26 +0800

    Remove extra listener

--- microservices/user-service/src/server.ts
+++ microservices/user-service/src/server.ts
@@ -3,8 +3,6 @@ import app from "./index";
 const port = process.env.PORT || 3219;
 const hostname = "::";
 
-app.listen(port);
-
 app.listen(Number(port), hostname, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
