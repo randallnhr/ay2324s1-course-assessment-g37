865a267fbedf2f1285dbf65e14aee5c17ddd06b9
wz2k <87819279+wz2k@users.noreply.github.com>
2023-10-09 11:31:48 +0800

    Change room count event emission

--- microservices/collaboration-service/src/index.ts
+++ microservices/collaboration-service/src/index.ts
@@ -29,7 +29,7 @@ io.on("connection", (socket) => {
   console.log(`Total rooms: ${socket.rooms.size - 1}`);
 
   // emit number of clients in room to ensure room is ready
-  socket.in(room).emit("room count", io.sockets.adapter.rooms.get(room)?.size);
+  io.in(room).emit("room count", io.sockets.adapter.rooms.get(room)?.size);
 
   socket.on("client code changes", (change) => {
     io.to(room).emit("server code changes", change);
