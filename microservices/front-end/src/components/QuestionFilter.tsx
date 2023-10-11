ea6d0ca99e259775db427c761eaa443e5066c908
heeeyi <e0772644@u.nus.edu>
2023-10-11 15:13:54 +0800

    Added a search bar

--- microservices/front-end/src/components/QuestionFilter.tsx
+++ microservices/front-end/src/components/QuestionFilter.tsx
@@ -4,6 +4,10 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 import styles from "./QuestionFilter.module.css";
 
 interface QuestionFilterProps {
@@ -92,6 +96,21 @@ const QuestionFilter: React.FC<QuestionFilterProps> = ({
           </Select>
         </FormControl>
       </Box>
+
+      {/* Added Search Bar */}
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 300 }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Questions"
+          inputProps={{ "aria-label": "search questions" }}
+        />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
     </div>
   );
 };
