1e5ed619496e7a125ecd7e3635226d86d96e64c1
rexcyrio <stefancyrio@gmail.com>
2023-10-11 09:18:43 +0800

    style: missing semicolon

--- microservices/front-end/src/components/types.ts
+++ microservices/front-end/src/components/types.ts
@@ -1,27 +1,27 @@
-export type QuestionComplexity = 'Easy' | 'Medium' | 'Hard'
+export type QuestionComplexity = "Easy" | "Medium" | "Hard";
 
 export interface Question {
-    _id: string;
-    title: string;
-    description: string;
-    categories: string[];
-    complexity: QuestionComplexity
+  _id: string;
+  title: string;
+  description: string;
+  categories: string[];
+  complexity: QuestionComplexity;
 }
 
 export interface User {
-    username: string;
-    displayName: string;
-    role: "basic" | "admin";
+  username: string;
+  displayName: string;
+  role: "basic" | "admin";
 }
 
 export interface NewQuestion {
-    title: string;
-    description: string;
-    categories: string[];
-    complexity: QuestionComplexity
+  title: string;
+  description: string;
+  categories: string[];
+  complexity: QuestionComplexity;
 }
 
 export type MatchRequest = {
-    userId: string
-    complexity: QuestionComplexity
-}
+  userId: string;
+  complexity: QuestionComplexity;
+};
