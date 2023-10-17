4b4023aa75de31ed9bfd7596db9467cc07a9caf8
wz2k <87819279+wz2k@users.noreply.github.com>
2023-10-17 21:29:56 +0800

    Modify cors origins

--- microservices/collaboration-service/src/index.ts
+++ microservices/collaboration-service/src/index.ts
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server(3111, {
   cors: {
-    origin: "http://127.0.0.1:5173",
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
   },
 });
 
