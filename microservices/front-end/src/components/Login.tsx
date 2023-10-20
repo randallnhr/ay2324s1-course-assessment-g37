a55490661493b02d9cf1fa6264c0090505dcd0a0
heeeyi <e0772644@u.nus.edu>
2023-10-20 20:09:12 +0800

    Revert "Revert "Resolve conflicts""
    This reverts commit 8323b8bf84a77de56eb5c782055914e371639764.

--- microservices/front-end/src/components/Login.tsx
+++ microservices/front-end/src/components/Login.tsx
@@ -5,11 +5,16 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../UserContext";
 import { User } from "./types";
+import { useAppDispatch, useAppSelector } from "../store/hook";
+import { fetchQuestions } from "../store/slices/questionsSlice";
 // components
 import PageContainer from "./container/PageContainer";
 import AuthLogin from "./auth/AuthLogin";
 
 const Login: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const questions = useAppSelector((state) => state.questions);
+
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const { currentUser, setCurrentUser } = useUserContext();
@@ -50,6 +55,10 @@ const Login: React.FC = () => {
         console.log("Current user set");
         console.log(currentUser.username);
         // navigate("/question-bank");
+
+        const fetchedQuestions = await dispatch(fetchQuestions());
+        console.log(fetchedQuestions);
+        console.log(questions);
 
         setSuccess("Successfully logged in!");
       }
