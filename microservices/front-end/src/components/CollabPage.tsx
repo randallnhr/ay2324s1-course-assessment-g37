eb931657989a08409d716c4713af1d1b103bc8ff
heeeyi <e0772644@u.nus.edu>
2023-10-23 19:48:56 +0800

    Same reason

--- microservices/front-end/src/components/CollabPage.tsx
+++ microservices/front-end/src/components/CollabPage.tsx
@@ -18,44 +18,22 @@ import { useAppSelector, useAppDispatch } from "../store/hook";
 import { fetchQuestions } from "../store/slices/questionsSlice";
 import { updateQuestionCategory } from "../store/slices/questionsSlice";
 import { selectSortedFilteredQuestions } from "../store/slices/questionFilterSlice";
-
-const allCategories = [
-  "Arrays",
-  "Strings",
-  "Hash Table",
-  "Math",
-  "Dynamic Programming",
-  "Sorting",
-  "Greedy",
-  "Depth-First Search",
-  "Binary Search",
-  "Databases",
-  "Breadth-First Search",
-  "Tree",
-  "Matrix",
-  "Two Pointers",
-  "Binary Tree",
-  "Bit Manipulation",
-  "Heap (Priority Queue)",
-  "Stack",
-  "Prefix Sum",
-  "Graph",
-  "Simulation",
-  "Design",
-  "Counting",
-  "Backtracking",
-  "Queue",
-  "Algorithms",
-  "Data Structures",
-  "Recursion",
-  "Brainteaser",
-  "Others",
-];
+import CollabQuestion from "./CollabQuestion";
 
 const CollabPage: React.FC = () => {
   const dispatch = useAppDispatch();
+  const sortedQuestions = useAppSelector(selectSortedFilteredQuestions);
 
   const navigate = useNavigate();
 
   const { currentUser, setCurrentUser } = useUserContext();
+
+  return (
+    <div>
+      <CollabQuestion 
+      questions={sortedQuestions}
+
+
+    </div>
+  )
 };
