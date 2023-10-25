a01d2dea2ac7cdf1541d282f44826f7a2e62573c
rexcyrio <stefancyrio@gmail.com>
2023-10-25 12:28:23 +0800

    fix: remove trailing comma

--- microservices/front-end/vite.config.ts
+++ microservices/front-end/vite.config.ts
@@ -4,7 +4,8 @@ import Svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd());
+
   return {
     plugins: [react(), Svgr()],
     server: {
@@ -15,6 +16,5 @@ export default defineConfig(({ mode }) => {
     watch: {
       usePolling: true,
     },
-  },
+  };
 });
-
